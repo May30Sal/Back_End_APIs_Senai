bd47d0f873c3f3a83833063e1c3fae0c4dda9737
MayaraSaldanha <maysal20@gmail.com>
2022-05-24 16:02:47 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoUc14/Controllers/LivroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoUc14.Repositories;
+
+namespace ProjetoUc14.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _LivroRepository;
+
+        public LivroController(LivroRepository LivroRepository)
+        {
+            _LivroRepository = LivroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_LivroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+    }
+}
---
+++ ProjetoUc14/Repositories/LivroRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoUc14.Contexts;
+using ProjetoUc14.Models;
+
+namespace ProjetoUc14.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+        public LivroRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
