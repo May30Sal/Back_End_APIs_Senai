e529527decf289c044f3756574562267054d4fbe
MayaraSaldanha <maysal20@gmail.com>
2022-05-25 18:30:03 -0300

    Crud completo de Livros

--- ProjetoUc14/Controllers/LivroController.cs
+++ ProjetoUc14/Controllers/LivroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoUc14.Models;
 using ProjetoUc14.Repositories;
 
 namespace ProjetoUc14.Controllers
@@ -30,6 +31,77 @@ namespace ProjetoUc14.Controllers
                 throw new Exception(e.Message);
             }
         }
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livro = _LivroRepository.BuscarPorId(id);
+                
+                if (livro == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(livro);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+               _LivroRepository.Cadastrar(livro);
+
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Atualizar(int id, Livro livro)
+        {
+            try
+            {
+                _LivroRepository.Atualizar(id, livro);
+
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _LivroRepository.Deletar(id);
+
+                return StatusCode(200);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
 
     }
 }
--- ProjetoUc14/Repositories/LivroRepository.cs
+++ ProjetoUc14/Repositories/LivroRepository.cs
@@ -15,5 +15,45 @@ namespace ProjetoUc14.Repositories
         {
             return _context.Livros.ToList();
         }
+
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Cadastrar(Livro Livro)
+        {
+            _context.Livros.Add(Livro); //insert no BD
+
+            _context.SaveChanges();
+        }
+
+        public void Atualizar(int id, Livro livro)
+        {
+            Livro livroBuscado = _context.Livros.Find(id);
+
+            if(livroBuscado != null)
+            {
+                livroBuscado.Titulo = livro.Titulo;
+
+                livroBuscado.Disponivel = livro.Disponivel;
+
+                livroBuscado.QuantidadePaginas = livro.QuantidadePaginas;
+            }
+
+            _context.Livros.Update(livroBuscado);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Livro livro = _context.Livros.Find(id);
+
+            _context.Livros.Remove(livro);
+
+            _context.SaveChanges();
+        }
+
+
     }
 }
